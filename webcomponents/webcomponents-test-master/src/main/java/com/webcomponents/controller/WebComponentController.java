067017ab5c05e23ac39bcd2b5a2d5fa12b287cd8
kathiravang <kathiravang@STGIDT-NEW-09.stg.com>
2019-09-19 12:49:50 +0530

    i implemented java doc for this respective controller. And,i start to implement logger implementation for this controller.

--- webcomponents/webcomponents-test-master/src/main/java/com/webcomponents/controller/WebComponentController.java
+++ webcomponents/webcomponents-test-master/src/main/java/com/webcomponents/controller/WebComponentController.java
@@ -1,16 +1,44 @@
 package com.webcomponents.controller;
 
+import java.io.Serializable;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class WebComponentController {
+@Scope("session")
+public class WebComponentController implements Serializable {
 	
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 861620058943228602L;
+	
+	/**
+	 * this is for the purpose of logger implementation.
+	 * And Logger is provided by this jar - (log4j-1.2.16.jar)
+	 * Logger Package is - (org.apache.log4j.Logger)
+	 */
+	public static final Logger LOGGER=Logger.getLogger(WebComponentController.class);
+	
+	/**
+	 * @author kathiravang
+	 * when the request is invoke that time this method is used to show the home.jsp page in browser
+	 * @return home.jsp
+	 */
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String home() {
-		System.out.println("home called");
+		
+		if(LOGGER.isInfoEnabled()) {
+			LOGGER.info("home method invoked");
+		}
 		return "home";
+		
 	}
 
 }
